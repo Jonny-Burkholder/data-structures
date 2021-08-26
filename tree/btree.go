5f52e496e5112d4f0588ed1c14b32c9a4f6f3931
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-08-26 10:57:36 -0400

    Heap, tree

--- tree/btree.go
+++ tree/btree.go
@@ -1,5 +1,9 @@
 package tree
 
+import (
+	"bytes"
+)
+
 //BTree has a root node pointer and implements basic BTree functions
 type BTree struct {
 	First *Node
@@ -25,5 +29,44 @@ func NewBTree() *BTree {
 
 //AddEntry takes a byte slice as an argument, creates a node, and adds it to the tree
 func (b *BTree) AddEntry(data []byte) {
-	//node := newNode(data)
+	//create node
+	node := newNode(data)
+	current := b.First
+	for { //no conditions because we'll return within the loop
+		//compare node to current node
+		res := bytes.Compare(node.Data, current.Data)
+		if res < 0 { //if new node is smaller than current node
+			if current.LeftChild == nil {
+				current.LeftChild = node
+				break
+			} else {
+				current = current.LeftChild
+			}
+		} else if res > 0 { //if new node is greater than current node
+			if current.RightChild == nil {
+				current.RightChild = node
+				break
+			} else {
+				current = current.RightChild
+			}
+		} else {
+			break
+		}
+	}
+}
+
+//Search takes a slice of bytes as an argument and searches the tree for that data, returning
+//the node containing the data, or a non-nil error if the data is not found in the tree
+func (b *BTree) Search(data []byte, n *Node) *Node {
+	if n == nil {
+		return n
+	}
+	res := bytes.Compare(data, n.Data)
+	if res < 0 {
+		return b.Search(data, n.LeftChild)
+	} else if res > 0 {
+		return b.Search(data, n.RightChild)
+	} else {
+		return n
+	}
 }
