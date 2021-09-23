807ba4ca1fe414d7dc5a477cea35169e43369e5d
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-09-23 13:45:55 -0400

    Create merge sort
    This is really more like a quicksort in disguise. Don't tell anybody

--- sort/mergesort.go
+++ sort/mergesort.go
@@ -1,1 +1,48 @@
 package sort
+
+//Merge sort splits up arrays and sorts them. Then merges them. You know, like in the name
+
+//Sort takes an input slice of integers and performs the merge sort operation on it
+func Sort(arr []int) []int {
+	if len(arr) == 1 {
+		return arr
+	}
+	//break array into smaller chunks
+	a := arr[:len(arr)-len(arr)/2]
+	b := arr[len(a):]
+	//sort the chunks
+	if len(a) > 1 {
+		a = Sort(a)
+	}
+	if len(b) > 1 {
+		b = Sort(b)
+	}
+	//merge the chunks back together
+	return Merge(a, b)
+}
+
+//Merge takes smaller, sorted arrays and merges them together
+func Merge(a, b []int) []int {
+	res := make([]int, len(a)+len(b))
+	//if the arrays are one int long, just compare them directly
+	if len(a) == 1 && len(b) == 1 {
+		if a[0] < b[0] {
+			res[0], res[1] = a[0], b[0]
+		} else {
+			res[0], res[1] = b[0], a[0]
+		}
+	} else {
+		res = make([]int, 0)
+		res = append(res, a...)
+		res = append(res, b...)
+		for i := range res {
+			for j := range res {
+				if res[i] < res[j] {
+					res[i], res[j] = res[j], res[i]
+				}
+			}
+		}
+	}
+
+	return res
+}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,1 +1,17 @@
 package sort
+
+import "testing"
+
+func TestSort(t *testing.T) {
+
+	arr := []int{5, 1, 8, 2, 7}
+	sorted := []int{1, 2, 5, 7, 8}
+	arr = Sort(arr)
+	for i, v := range arr {
+
+		if v != sorted[i] {
+			t.Errorf("Array not sorted. Wanted %v, got %v", sorted, arr)
+			break
+		}
+	}
+}
